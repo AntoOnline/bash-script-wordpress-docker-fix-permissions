f96a57765c5cbd2bfaae1dcf970733e3d4efa879
Anthony Mastoroudes <anthony.mast@gmail.com>
2023-04-25 19:21:17 +1000

    first commit

---
+++ readme.md
@@ -0,0 +1,32 @@
+# WordPress Permissions Fix Script
+
+This script can be used to fix the file and directory permissions for multiple running WordPress instances running in Docker containers.
+
+## Prerequisites
+
+To use this script, you will need to have the following installed on your system:
+
+- Docker
+
+## Usage
+
+To use this script, follow these steps:
+
+1. Download the `fix-wordpress-permissions.sh` file to your system.
+2. Open the terminal and navigate to the directory where the file is downloaded.
+3. Run the following command to make the script executable: `chmod +x fix-wordpress-permissions.sh`
+4. Run the script using the following command: `./fix-wordpress-permissions.sh`
+
+The script will automatically detect all running WordPress instances in Docker containers and fix the permissions for each instance. The script will also provide information about the status of each instance being fixed during the process.
+
+## Troubleshooting
+
+If the script is not working as expected, try the following troubleshooting steps:
+
+- Make sure that Docker is installed and running.
+- Check that the WordPress containers are running and are based on the official `wordpress` image.
+- Make sure that you have the necessary permissions to execute the script.
+
+## Disclaimer
+
+This script is provided as-is and is intended for use at your own risk. Always backup your data before making changes to your file and directory permissions. The author is not responsible for any damage or loss caused by the use of this script.
---
+++ wordpress-docker-fix-permissions.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo "Getting the container names of running WordPress instances..."
+
+# Get the container names of running WordPress instances
+container_names=$(docker ps -aqf "ancestor=wordpress" --format "{{.Names}}")
+
+echo "Found the following WordPress instances:"
+echo "$container_names"
+
+# Loop through each container name and fix the WordPress permissions
+for container_name in $container_names; do
+
+    echo "Fixing WordPress permissions inside container: $container_name"
+
+    # Set the WordPress root directory inside the container
+    wordpress_root="/var/www/html/"
+
+    # Change directory to the WordPress root directory inside the container
+    docker exec -it $container_name bash -c "cd $wordpress_root"
+
+    echo "Setting directory permissions..."
+    # Set the correct file permissions for directories
+    docker exec -it $container_name bash -c "find $wordpress_root -type d -exec chmod 755 {} \;"
+
+    echo "Setting file permissions..."
+    # Set the correct file permissions for files
+    docker exec -it $container_name bash -c "find $wordpress_root -type f -exec chmod 644 {} \;"
+
+    echo "Setting ownership..."
+    # Set the correct ownership for all files and directories
+    docker exec -it $container_name bash -c "chown -R www-data:www-data $wordpress_root"
+
+    echo "Setting group permissions..."
+    # Set the correct group permissions for wp-content directory and its subdirectories
+    docker exec -it $container_name bash -c "chown -R :www-data ${wordpress_root}wp-content"
+    docker exec -it $container_name bash -c "find ${wordpress_root}wp-content -type d -exec chmod 775 {} \;"
+    docker exec -it $container_name bash -c "find ${wordpress_root}wp-content -type f -exec chmod 664 {} \;"
+
+    echo "Done fixing WordPress permissions inside container: $container_name"
+
+done
+
+echo "All WordPress instances have been fixed."
